3c89316349db4fc7bd0c31666ae263d89c823d6f
Geir Arne Tangen <Geir.A.Tangen@sintef.no>
2022-02-08 13:35:15 +0100

    ENH: Initial code for segments UI.

--- Liver/Liver.py
+++ Liver/Liver.py
@@ -146,11 +146,16 @@ class LiverWidget(ScriptedLoadableModuleWidget):
     resectionsUI= slicer.util.loadUI(self.resourcePath('UI/ResectionsWidget.ui'))
     resectionsUI.setMRMLScene(slicer.mrmlScene)
 
+    segmentsUI= slicer.util.loadUI(self.resourcePath('UI/segments.ui'))
+    segmentsUI.setMRMLScene(slicer.mrmlScene)
+
     self.layout.addWidget(distanceMapsUI)
     self.layout.addWidget(resectionsUI)
+    self.layout.addWidget(segmentsUI)
 
     self.distanceMapsWidget = slicer.util.childWidgetVariables(distanceMapsUI)
     self.resectionsWidget = slicer.util.childWidgetVariables(resectionsUI)
+    self.segmentsWidget = slicer.util.childWidgetVariables(segmentsUI)
 
     # Add a spacer at the botton to keep the UI flowing from top to bottom
     spacerItem = qt.QSpacerItem(0,0, qt.QSizePolicy.Minimum, qt.QSizePolicy.MinimumExpanding)
