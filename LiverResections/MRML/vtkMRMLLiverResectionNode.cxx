1bd4230fce313064e9ba96650190345c45428849
Geir Arne Tangen <Geir.A.Tangen@sintef.no>
2022-03-20 17:11:29 +0100

    BUG: Fix for macOS build (#121).

--- LiverResections/MRML/vtkMRMLLiverResectionNode.cxx
+++ LiverResections/MRML/vtkMRMLLiverResectionNode.cxx
@@ -55,8 +55,8 @@ vtkMRMLLiverResectionNode::vtkMRMLLiverResectionNode()
   :Superclass(), TargetOrganModelNode(nullptr), DistanceMapVolumeNode(nullptr),
    State(ResectionState::Initialization), InitMode(InitializationMode::Flat),
    ResectionMargin(0.0), UncertaintyMargin(0.0), ClipOut(false), WidgetVisibility(true),
-   InterpolatedMargins(false), ResectionColor({1.0f, 1.0f, 1.0f}),
-   ResectionMarginColor({1.0f, 0.0f, 0.0f}), UncertaintyMarginColor({1.0f, 1.0f, 0.0f}),
+   InterpolatedMargins(false), ResectionColor{1.0f, 1.0f, 1.0f},
+   ResectionMarginColor{1.0f, 0.0f, 0.0f}, UncertaintyMarginColor{1.0f, 1.0f, 0.0f},
    ResectionOpacity(1.0f), GridVisibility(false)
 {
 }
